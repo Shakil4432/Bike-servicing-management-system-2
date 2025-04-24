cd5feb869f4032babcfa9249cffde290274eed1c
Shakil4432 <shakil4432@gmail.com>
2025-04-24 16:47:14 +0600

    deploy

--- prisma/migrations/20250423083941_update_files/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `completionDate` to the `service_records` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "service_records" ADD COLUMN     "completionDate" TIMESTAMP(3) NOT NULL;
--- prisma/migrations/20250423084148_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "service_records" ALTER COLUMN "completionDate" DROP NOT NULL;
